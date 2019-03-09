568df32ce908280dbdc571f4d1f48ebd72515d8e
isdev0 <isdev@rambler.ru>
2019-03-10 01:27:05 +0300

    package added

--- sandbox/src/main/java/TestSample1.java
+++
@@ -1,7 +0,0 @@
-public class TestSample1 {
-
-   public static void main(String[] args) {
-      System.out.println("Hello, world!");
-   }
-
-}
---
+++ sandbox/src/main/java/ru/isdev/sandbox/TestSample.java
@@ -0,0 +1,11 @@
+package ru.isdev.sandbox;
+
+public class TestSample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        System.out.println();
+    }
+
+}
