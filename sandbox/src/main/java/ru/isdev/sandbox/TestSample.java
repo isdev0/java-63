9e1a6a625600d141de345609587738c84e0d2cae
isdev0 <isdev@rambler.ru>
2019-03-10 02:58:38 +0300

    Distance and Point classes added

--- sandbox/src/main/java/ru/isdev/sandbox/TestSample.java
+++ sandbox/src/main/java/ru/isdev/sandbox/TestSample.java
@@ -6,25 +6,16 @@ public class TestSample {
 
         hello("woolf");
 
-        double l = 12;
-        System.out.println("The area od square with a side " + l + " = " + area(l) );
+        Square square = new Square(15);
+        System.out.println("The area of square with a side " + square.l + " = " + square.area() );
 
-        double a = 123;
-        double b = 321;
-        System.out.println("The area of rectangle with a side " + a + " and " + b + " = " + area(a,b) );
+        Rectangle rectangle = new Rectangle(123,131);
+        System.out.println("The area of rectangle with a side " + rectangle.a + " and " + rectangle.b + " = " + rectangle.area() );
 
     }
 
     public static void hello(String userName){
         System.out.println("Hello, " + userName + "!");
-    }
-
-    public static double area(double len){
-        return len * len;
-    }
-
-    public static double area(double a, double b){
-        return a * b;
     }
 
 }
