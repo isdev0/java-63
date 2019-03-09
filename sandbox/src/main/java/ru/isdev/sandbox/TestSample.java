a27a8f4d62ab4678a7c752df3c381ccb385fcb83
isdev0 <isdev@rambler.ru>
2019-03-10 01:56:21 +0300

    some sample functions added

--- sandbox/src/main/java/ru/isdev/sandbox/TestSample.java
+++ sandbox/src/main/java/ru/isdev/sandbox/TestSample.java
@@ -3,9 +3,28 @@ package ru.isdev.sandbox;
 public class TestSample {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
 
-        System.out.println();
+        hello("woolf");
+
+        double l = 12;
+        System.out.println("The area od square with a side " + l + " = " + area(l) );
+
+        double a = 123;
+        double b = 321;
+        System.out.println("The area of rectangle with a side " + a + " and " + b + " = " + area(a,b) );
+
+    }
+
+    public static void hello(String userName){
+        System.out.println("Hello, " + userName + "!");
+    }
+
+    public static double area(double len){
+        return len * len;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 
 }
