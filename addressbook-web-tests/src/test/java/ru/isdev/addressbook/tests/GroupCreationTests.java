573d613207687ec9144c7a5f1272df88430db49a
isdev0 <isdev@rambler.ru>
2019-04-22 03:04:45 +0300

    comparator added to the GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/isdev/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/isdev/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.isdev.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -11,6 +12,8 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
+
+        int maxIndex;
 
         app.getNavigationHelper().goToTheGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
@@ -22,12 +25,16 @@ public class GroupCreationTests extends TestBase {
 
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int maxIndex = 0;
+        /* saved for Java 6 compatible code
+        maxIndex = 0;
         for(GroupData g: after){
             if(g.getId() > maxIndex){
                 maxIndex = g.getId();
             }
         }
+        */
+
+        maxIndex = after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId();
 
         group.setId(maxIndex);
         before.add(group);
