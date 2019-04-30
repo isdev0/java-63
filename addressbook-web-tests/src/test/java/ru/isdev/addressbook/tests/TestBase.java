fde13a80439c3d65a05963cddf72b8ed72ba1107
isdev0 <isdev@rambler.ru>
2019-05-01 01:56:55 +0300

    Test run optimization

--- addressbook-web-tests/src/test/java/ru/isdev/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/isdev/addressbook/tests/TestBase.java
@@ -1,20 +1,20 @@
 package ru.isdev.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.isdev.addressbook.appmanager.ApplicationManger;
 
 public class TestBase {
 
-    protected final ApplicationManger app = new ApplicationManger(BrowserType.CHROME);
+    protected static final ApplicationManger app = new ApplicationManger(BrowserType.CHROME);
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
