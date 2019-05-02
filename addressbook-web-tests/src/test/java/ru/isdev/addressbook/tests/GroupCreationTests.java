434ec739fa56c2fc8fa7ed551e5a5ca8452d4c3c
isdev0 <isdev@rambler.ru>
2019-05-02 20:42:54 +0300

    Hamcrest added. GroupTests refactoring - Groups model added

--- addressbook-web-tests/src/test/java/ru/isdev/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/isdev/addressbook/tests/GroupCreationTests.java
@@ -1,46 +1,40 @@
 package ru.isdev.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.isdev.addressbook.model.GroupData;
+import ru.isdev.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions(){
+        app.goTo().theGroupPage();
+    }
 
     @Test
     public void testGroupCreation() throws Exception {
 
-        int maxIndex;
+        Groups before = app.group().all();
 
-        app.goTo().theGroupPage();
-        Set<GroupData> before = app.group().all();
-
-        GroupData group = new GroupData().withName("TetGroup1_name").withHeader("TestGroup1_header");
-
+        GroupData group = new GroupData()
+                .withName("TetGroup1_name")
+                .withHeader("TestGroup1_header");
         app.group().create(group);
-        Set<GroupData> after = app.group().all();
 
-        Assert.assertEquals(after.size(), before.size() + 1);
+        Groups after = app.group().all();
 
-        // Java 6 compatible code
-        /*
-        maxIndex = 0;
-        for(GroupData g: after){
-            if(g.getId() > maxIndex){
-                maxIndex = g.getId();
-            }
-        }
-        */
-
-        // Java 8 compatible code
-        //maxIndex = after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId();
-        maxIndex = after.stream().mapToInt(GroupData::getId).max().getAsInt();
-
-        group.withId(maxIndex);
-        before.add(group);
-
-        Assert.assertEquals(before, after);
+        assertThat(after.size(), equalTo(before.size() + 1));
+        assertThat(after, equalTo(
+                before.withAdded(
+                        group.withId(
+                                after.stream().mapToInt(GroupData::getId).max().getAsInt()
+                        )
+                )
+            )
+        );
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/isdev/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/isdev/addressbook/tests/GroupModificationTests.java
@@ -1,11 +1,13 @@
 package ru.isdev.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.isdev.addressbook.model.GroupData;
+import ru.isdev.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupModificationTests extends TestBase {
 
@@ -18,7 +20,8 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification(){
 
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
+
         GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData()
                 .withId(modifiedGroup.getId())
@@ -27,12 +30,14 @@ public class GroupModificationTests extends TestBase {
                 .withFooter("TestGroup1_Edit_footer");
         app.group().modify(group);
 
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size());
+        Groups after = app.group().all();
 
-        before.remove(modifiedGroup);
-        before.add(group);
-        Assert.assertEquals(before, after);
+        assertEquals(after.size(), before.size());
+        assertThat(after, equalTo(before
+                .without(modifiedGroup)
+                .withAdded(group)
+                )
+        );
     }
 
 }
